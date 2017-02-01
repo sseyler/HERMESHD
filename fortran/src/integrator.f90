e845143d36a6fba30efecfaec4fd8897193ede28
Sean Seyler <slseyler@asu.edu>
2017-01-31 18:21:54 -0700

    Added separate modules for flux, integration, step_prep

---
+++ fortran/src/integrator.f90
@@ -0,0 +1,188 @@
+module integrator
+
+    use input!, only : nx,ny,nz
+    use parameters!, only : nQ,nbasis,Q_r0,Q_r1,Q_r2,Q_r3
+    use helpers
+
+    use prepare_step
+    use sources
+    use flux
+
+    !===========================================================================
+    ! ABSTRACT INTERFACE to subroutine for temporal integration
+    !-----------------------------------------------------------------
+    abstract interface
+        subroutine update_ptr (Q_io, Q_1, Q_2)
+            use input, only : nx,ny,nz
+            use parameters, only : nQ,nbasis
+
+            real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_io
+            real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_1, Q_2
+        end subroutine update_ptr
+    end interface
+    !---------------------------------------------------------------------------
+
+
+    !===========================================================================
+    ! Initialize pointer to temporal integration subroutine
+    !-----------------------------------------------------------------
+    procedure (update_ptr), pointer :: update => null ()
+    !---------------------------------------------------------------------------
+
+
+contains
+
+    !===========================================================================
+    ! Select user-specified integration method at runtime
+    !-----------------------------------------------------------------
+    subroutine select_integrator(name, integrator)
+        implicit none
+        character(*), intent(in) :: name
+        procedure(update_ptr), pointer :: integrator
+
+        select case(name)
+            case('heun')
+                call mpi_print(iam, 'Selected 2nd-order Runga-Kutta (Heun) integration')
+                integrator => RK2
+            case('shu-osher')
+                call mpi_print(iam, 'Selected 3rd-order Runga-Kutta (Shu-Osher) integration')
+                integrator => RK3
+            case default
+                call mpi_print(iam, 'Defaulting to 2nd-order Runga-Kutta (Heun) integration')
+                integrator => RK2
+        end select
+    end subroutine
+    !---------------------------------------------------------------------------
+
+
+    !===========================================================================
+    ! Temporal integration subroutines (subject to change!)
+    !-----------------------------------------------------------------
+    subroutine RK2(Q_io, Q_1, Q_2)
+        implicit none
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_io
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_1, Q_2
+
+        call euler_step(Q_io, Q_1)
+        call euler_step(Q_1, Q_2)
+        Q_io = 0.5 * ( Q_io + Q_2 )
+    end subroutine RK2
+
+    !----------------------------------------------------
+    subroutine RK3(Q_io, Q_1, Q_2)
+        implicit none
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_io
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_1, Q_2
+
+        call euler_step(Q_io, Q_1)
+        call euler_step(Q_1, Q_2)
+        Q_1 = 0.75*Q_io + 0.25*Q_2
+
+        call euler_step(Q_1, Q_2)  ! re-use the second array
+        Q_io = c1d3 * ( Q_io + 2.0*Q_2 )
+    end subroutine RK3
+    !---------------------------------------------------------------------------
+
+
+    !===========================================================================
+    ! Explicit Euler integration step
+    !-----------------------------------------------------------------
+    subroutine euler_step(Q_in, Q_out)
+        implicit none
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_in
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(out) :: Q_out
+
+        call prep_advance(Q_in)
+        call calc_rhs(Q_in)
+        call advance_time_level_gl(Q_in, Q_out)
+    end subroutine euler_step
+
+    !----------------------------------------------------
+    subroutine prep_advance(Q_io)
+        implicit none
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_io
+
+        if (ieos .eq. 1 .or. ieos .eq. 2) call limiter(Q_io)
+        call prepare_exchange(Q_io)
+        call exchange_flux
+        call apply_boundaries
+    end subroutine prep_advance
+
+    !----------------------------------------------------
+    subroutine calc_rhs(Q_io)
+        implicit none
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(inout) :: Q_io
+
+        call flux_cal(Q_io)
+        call innerintegral(Q_io)
+        call glflux
+        call source_calc(Q_io)
+    end subroutine calc_rhs
+
+    !----------------------------------------------------
+    subroutine advance_time_level_gl(Q_in, Q_out)
+        implicit none
+        integer i,j,k,ieq,ir
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(in) :: Q_in
+        real, dimension(nx,ny,nz,nQ,nbasis), intent(out) :: Q_out
+
+        do k = 1,nz
+        do j = 1,ny
+        do i = 1,nx
+
+            do ieq = 1,nQ
+            do ir=1,nbasis
+                Q_out(i,j,k,ieq,ir) =                                           &
+                    Q_in(i,j,k,ieq,ir) - dt*( glflux_r(i,j,k,ieq,ir)            &
+                                            - source_r(i,j,k,ieq,ir) )
+            end do
+            end do
+
+            do ieq = 1,nQ
+                if ( Q_out(i,j,k,ieq,1) .ne. Q_out(i,j,k,ieq,1)) then
+                    print *,'NaN. Bailing out...'
+                    print *,'  xc  =',xc(i),'  yc  =',yc(j),'  zc  =',zc(k),'  ieq  =',ieq
+                    call exit(-1)
+                endif
+            end do
+
+        end do
+        end do
+        end do
+
+        ! do ir=1,nbasis
+        ! do ieq = 1,nQ
+        !     do k = 1,nz
+        !     do j = 1,ny
+        !     do i = 1,nx
+        !         Q_out(i,j,k,ieq,ir) =                                           &
+        !             Q_in(i,j,k,ieq,ir) - dt*( glflux_r(i,j,k,ieq,ir)            &
+        !                                     - source_r(i,j,k,ieq,ir) )
+        !         if ( Q_out(i,j,k,ieq,1) .ne. Q_out(i,j,k,ieq,1)) then
+        !             print *,'NaN. Bailing out...'
+        !             print *,'  xc  =',xc(i),'  yc  =',yc(j),'  zc  =',zc(k),'  ieq  =',ieq
+        !             call exit(-1)
+        !         endif
+        !     end do
+        !     end do
+        !     end do
+        ! end do
+        ! end do
+        ! do ieq = 1,nQ
+        !     do k = 1,nz
+        !     do j = 1,ny
+        !     do i = 1,nx
+        !         if ( Q_out(i,j,k,ieq,1) .ne. Q_out(i,j,k,ieq,1)) then
+        !             print *,'NaN. Bailing out...'
+        !             print *,'  xc  =',xc(i),'  yc  =',yc(j),'  zc  =',zc(k),'  ieq  =',ieq
+        !             call exit(-1)
+        !         endif
+        !     end do
+        !     end do
+        !     end do
+        ! end do
+
+    end subroutine advance_time_level_gl
+    !---------------------------------------------------------------------------
+
+end module integrator
---
+++ fortran/src/prepare_step.f90
@@ -0,0 +1,171 @@
+module prepare_step
+
+use parameters
+use helpers
+use boundaries
+
+contains
+
+    !===========================================================================
+    ! prepare_exchange : set internal BCs from field variables
+    !------------------------------------------------------------
+    subroutine prepare_exchange(Q_r)
+        integer ieq, i, j, k, ipnt
+        real, dimension(nx,ny,nz,nQ,nbasis) :: Q_r
+
+        do ieq = 1,nQ
+        do j = 1,ny
+        do i = 1,nx
+            do ipnt=1,nface
+                Qzlow_int(i,j,ipnt,ieq) = sum(bfvals_zm(ipnt,1:nbasis)*Q_r(i,j,1,ieq,1:nbasis))
+                Qzhigh_int(i,j,ipnt,ieq) = sum(bfvals_zp(ipnt,1:nbasis)*Q_r(i,j,nz,ieq,1:nbasis))
+            end do
+        end do
+        end do
+        end do
+
+        do ieq = 1,nQ
+        do k = 1,nz
+        do i = 1,nx
+            do ipnt=1,nface
+                Qylow_int(i,k,ipnt,ieq) = sum(bfvals_ym(ipnt,1:nbasis)*Q_r(i,1,k,ieq,1:nbasis))
+                Qyhigh_int(i,k,ipnt,ieq) = sum(bfvals_yp(ipnt,1:nbasis)*Q_r(i,ny,k,ieq,1:nbasis))
+            end do
+        end do
+        end do
+        end do
+
+        do ieq = 1,nQ
+        do k = 1,nz
+        do j = 1,ny
+            do ipnt=1,nface
+                Qxlow_int(j,k,ipnt,ieq) = sum(bfvals_xm(ipnt,1:nbasis)*Q_r(1,j,k,ieq,1:nbasis))
+                Qxhigh_int(j,k,ipnt,ieq) = sum(bfvals_xp(ipnt,1:nbasis)*Q_r(nx,j,k,ieq,1:nbasis))
+            end do
+        end do
+        end do
+        end do
+    end subroutine
+    !---------------------------------------------------------------------------
+
+
+    !===========================================================================
+    ! exchange_flux : exchange fluxes between MPI domains
+    !------------------------------------------------------------
+    subroutine exchange_flux
+        integer mpi_size
+
+        call MPI_BARRIER(cartcomm,ierr)
+
+        !---------------------------------------------
+         mpi_size = ny*nz*nface*nQ
+
+        if (nbrs(EAST) .ne. MPI_PROC_NULL) then
+            call MPI_ISend(Qxhigh_int,mpi_size,MPI_TT,nbrs(EAST),0,cartcomm,reqs(1),ierr)
+        endif
+
+        if (nbrs(WEST) .ne. MPI_PROC_NULL) then
+            call MPI_IRecv(Qxlow_ext,mpi_size,MPI_TT,nbrs(WEST),0,cartcomm,reqs(2),ierr)
+        endif
+
+        if (nbrs(EAST) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(1),stats(:,1),ierr)
+            call MPI_IRecv(Qxhigh_ext,mpi_size,MPI_TT,nbrs(EAST),0,cartcomm,reqs(3),ierr)
+        endif
+
+        if (nbrs(WEST) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(2),stats(:,2),ierr)
+            call MPI_ISend(Qxlow_int,mpi_size,MPI_TT,nbrs(WEST),0,cartcomm,reqs(4),ierr)
+        endif
+
+        if (nbrs(EAST) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(3),stats(:,3),ierr)
+        endif
+
+        if (nbrs(WEST) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(4),stats(:,4),ierr)
+        endif
+
+        if (mpi_P == 1 .and. xlobc == 'outflow') then
+            Qxlow_ext = Qxlow_int
+        end if
+
+        if (mpi_P == mpi_nx .and. xhibc == 'outflow') then
+            Qxhigh_ext = Qxhigh_int
+        end if
+
+        !---------------------------------------------
+        mpi_size = nface*nx*nz*nQ
+
+        if (nbrs(NORTH) .ne. MPI_PROC_NULL) then
+            call MPI_ISend(Qyhigh_int,mpi_size,MPI_TT,nbrs(NORTH),0,cartcomm,reqs(1),ierr)
+        endif
+
+        if (nbrs(SOUTH) .ne. MPI_PROC_NULL) then
+            call MPI_IRecv(Qylow_ext,mpi_size,MPI_TT,nbrs(SOUTH),0,cartcomm,reqs(2),ierr)
+        endif
+
+        if (nbrs(NORTH) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(1),stats(:,1),ierr)
+            call MPI_IRecv(Qyhigh_ext,mpi_size,MPI_TT,nbrs(NORTH),0,cartcomm,reqs(3),ierr)
+        endif
+
+        if (nbrs(SOUTH) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(2),stats(:,2),ierr)
+            call MPI_ISend(Qylow_int,mpi_size,MPI_TT,nbrs(SOUTH),0,cartcomm,reqs(4),ierr)
+        endif
+
+        if (nbrs(NORTH) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(3),stats(:,3),ierr)
+        endif
+
+        if (nbrs(SOUTH) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(4),stats(:,4),ierr)
+        endif
+
+        if (mpi_Q == 1 .and. ylobc == 'outflow') then
+            Qylow_ext = Qylow_int
+        end if
+
+        if (mpi_Q == mpi_ny .and. yhibc == 'outflow') then
+            Qyhigh_ext = Qyhigh_int
+        end if
+
+        !---------------------------------------------
+        mpi_size = nface*nx*ny*nQ
+
+        if (nbrs(UP) .ne. MPI_PROC_NULL) then
+            call MPI_ISend(Qzhigh_int,mpi_size,MPI_TT,nbrs(UP),0,cartcomm,reqs(1),ierr)
+        endif
+        if (nbrs(DOWN) .ne. MPI_PROC_NULL) then
+            call MPI_IRecv(Qzlow_ext,mpi_size,MPI_TT,nbrs(DOWN),0,cartcomm,reqs(2),ierr)
+        endif
+        if (nbrs(UP) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(1),stats(:,1),ierr)
+            call MPI_IRecv(Qzhigh_ext,mpi_size,MPI_TT,nbrs(UP),0,cartcomm,reqs(3),ierr)
+        endif
+        if (nbrs(DOWN) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(2),stats(:,2),ierr)
+            call MPI_ISend(Qzlow_int,mpi_size,MPI_TT,nbrs(DOWN),0,cartcomm,reqs(4),ierr)
+        endif
+        if (nbrs(UP) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(3),stats(:,3),ierr)
+        endif
+
+        if (nbrs(DOWN) .ne. MPI_PROC_NULL) then
+            call MPI_Wait(reqs(4),stats(:,4),ierr)
+        endif
+
+
+        if (mpi_R == 1 .and. zlobc == 'outflow') then
+            Qzlow_ext = Qzlow_int
+        end if
+
+        if (mpi_R == mpi_nz .and. zhibc == 'outflow') then
+            Qzhigh_ext = Qzhigh_int
+        end if
+
+    end subroutine
+    !---------------------------------------------------------------------------
+
+end module prepare_step
