ceadbdb47eb91057e85336218d67fa495734a992
Sean Seyler <slseyler@asu.edu>
2017-04-03 14:44:14 -0700

    Fixed perseusvars to export correct paths for MKL

--- fortran/src/perseusvars.sh
+++ fortran/src/perseusvars.sh
@@ -7,6 +7,11 @@ module load intel/17.0/fortran/64
 
 
 # Exports to set up paths Intel MKL
-export MKLROOT=/nfs/packages/opt/Linux_x86_64/intel/17.0/fortran/mkl
+export MKLROOT=/nfs/packages/opt/Linux_x86_64/intel/17.0/mkl
+export MKL_TARGET_ARCH=intel64_lin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${MKLROOT}/lib/${MKL_TARGET_ARCH}
+
+
+export MKLROOT=/nfs/packages/opt/Linux_x86_64/intel/13.0/mkl
 export MKL_TARGET_ARCH=intel64_lin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${MKLROOT}/lib/${MKL_TARGET_ARCH}
